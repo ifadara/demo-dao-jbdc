e325caf2c7fcd935ee8488a2329b73109379e55a
icaro.silva <icaro.silva@venhapranuvem.com.br>
2023-02-05 20:31:24 -0300

    Create project and git repository

---
+++ src/db/DbIntegityException.java
@@ -0,0 +1,12 @@
+package db;
+
+public class DbIntegityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntegityException(String msg) {
+		super(msg);
+	}
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author icaro.silva
+ *
+ */
+module demo_dao_jdbc {
+	requires java.sql;
+}
